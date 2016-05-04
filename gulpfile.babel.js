03bf21e071756d8958138b9ab33af57718846c0e
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2016-05-04 23:53:21 +0530

    adding some files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -18,8 +18,8 @@ const paths = {
     fonts: 'src/fonts/*'
 };
 
-gulp.task('clean', () => {
-    return del(['dist']);
+gulp.task('clean:dist', () => {
+    return del.sync('dist');
 });
 
 gulp.task('serve', () => {
@@ -56,9 +56,18 @@ gulp.task('scripts', () => {
         .pipe(browserSync.reload({ stream: true }));
 });
 
+gulp.task('copy:bootstrap', () => {
+    gulp.src('bower_components/bootstrap/dist/css/*.css').pipe(gulp.dest('dist/css'));
+    gulp.src('bower_components/bootstrap/dist/fonts/*.*').pipe(gulp.dest('dist/fonts'));
+    gulp.src('bower_components/bootstrap/dist/js/*.js').pipe(gulp.dest('dist/scripts'));
+});
+
 gulp.task('copy:images', () => {
-    return gulp.src(paths.images, {dot: true})
+    gulp.src(paths.images, {dot: true})
         .pipe(gulp.dest('dist/images'))
+        .pipe(browserSync.reload({ stream: true }));
+    gulp.src(paths.fonts, {dot: true})
+        .pipe(gulp.dest('dist/fonts'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
@@ -67,8 +76,9 @@ gulp.task('watch', () =>  {
     gulp.watch(paths.scripts, ['scripts']);
     gulp.watch(paths.jade, ['jade']);
     gulp.watch(paths.images, ['copy:images']);
+    gulp.watch('./dist/index.html', browserSync.reload);
 });
 
 gulp.task('default', () => {
-    runSequence(['clean', 'sass', 'jade', 'copy:images', 'serve', 'watch']);
+    runSequence(['clean:dist', 'sass', 'scripts', 'jade', 'copy:images', 'copy:bootstrap', 'serve', 'watch']);
 });
