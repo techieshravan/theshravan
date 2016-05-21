53c028e7d8d0ca07539b53e8dcd22f8db09ac2db
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2016-05-21 15:45:43 +0530

    small changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -24,10 +24,10 @@ gulp.task('clean:dist', () => {
 
 gulp.task('serve', () => {
     browserSync.init({
-    server: {
-        baseDir: 'dist'
-    }
-});
+        server: {
+            baseDir: 'dist'
+        }
+    });
 });
 
 gulp.task('useref', () => {
@@ -44,49 +44,65 @@ gulp.task('sass', () => {
         .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('jade', () => {
-    return gulp.src('src/index.jade')
-        .pipe(jade({ pretty: true }))
-        .pipe(gulp.dest('dist/'));
-});
-
 gulp.task('scripts', () => {
     return gulp.src(paths.scripts)
         .pipe(gulp.dest('dist/js/'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('copy:bootstrap', () => {
-    gulp.src('bower_components/bootstrap/dist/css/*.css').pipe(gulp.dest('dist/css'));
-    gulp.src('bower_components/bootstrap/dist/fonts/*.*').pipe(gulp.dest('dist/fonts'));
-    gulp.src('bower_components/bootstrap/dist/js/*.js').pipe(gulp.dest('dist/js'));
-    
-    gulp.src('bower_components/jquery/dist/*.js').pipe(gulp.dest('dist/js'));
-
-    gulp.src('src/font-awesome/css/*.*').pipe(gulp.dest('dist/font-awesome/css'));
-    gulp.src('src/font-awesome/fonts/*.*').pipe(gulp.dest('dist/font-awesome/fonts'));
-
-    gulp.src('src/css/animate.min.css').pipe(gulp.dest('dist/css'));
+gulp.task('jade', () => {
+    return gulp.src('src/index.jade')
+        .pipe(jade({ pretty: true }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('copy:images', () => {
     gulp.src(paths.images, {dot: true})
-    .pipe(gulp.dest('dist/img'))
-    .pipe(browserSync.reload({ stream: true }));
-
-gulp.src(paths.fonts, {dot: true})
-    .pipe(gulp.dest('dist/fonts'))
-    .pipe(browserSync.reload({ stream: true }));
+        .pipe(gulp.dest('dist/img'))
+        .pipe(browserSync.reload({ stream: true }));
 });
+
+gulp.task('copy:fonts', () => {
+    gulp.src(paths.fonts, {dot: true})
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe(browserSync.reload({ stream: true }));
+
+    gulp.src('src/font-awesome/css/*.*')
+        .pipe(gulp.dest('dist/font-awesome/css'));
+
+    gulp.src('src/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('dist/font-awesome/fonts'));
+});
+
+gulp.task('copy:bootstrap', () => {
+    gulp.src('bower_components/bootstrap/dist/css/*.css')
+        .pipe(gulp.dest('dist/css'));
+
+    gulp.src('bower_components/bootstrap/dist/fonts/*.*')
+        .pipe(gulp.dest('dist/fonts'));
+
+    gulp.src('bower_components/bootstrap/dist/js/*.js')
+        .pipe(gulp.dest('dist/js'));
+    
+    gulp.src('bower_components/jquery/dist/*.js')
+        .pipe(gulp.dest('dist/js'));
+
+    gulp.src('src/css/animate.min.css')
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy', ['copy:images', 'copy:fonts', 'copy:bootstrap']);
 
 gulp.task('watch', () =>  {
     gulp.watch('src/scss/*.scss', ['sass']);
     gulp.watch(paths.scripts, ['scripts']);
     gulp.watch(paths.jade, ['jade']);
     gulp.watch(paths.images, ['copy:images']);
-    gulp.watch('./dist/index.html', browserSync.reload);
+    gulp.watch(paths.fonts, ['copy:fonts']);
+    gulp.watch('dist/index.html', browserSync.reload);
 });
 
 gulp.task('default', () => {
-    runSequence(['clean:dist', 'sass', 'scripts', 'jade', 'copy:images', 'copy:bootstrap', 'serve', 'watch']);
+    runSequence(['clean:dist', 'sass', 'scripts', 'jade', 'copy', 'serve', 'watch']);
 });
